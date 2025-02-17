d7c16d45d32d2850f696bf74db933d91dfba57ed
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-02-17 07:25:59 +0100

    routing

--- front/src/theme.tsx
+++ front/src/theme.tsx
@@ -1,7 +1,22 @@
+import * as React from 'react';
+import {
+  Link as RouterLink,
+  LinkProps as RouterLinkProps,
+} from 'react-router';
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
+import { LinkProps } from '@mui/material/Link';
 
 // A custom theme for this app
+const LinkBehavior = React.forwardRef<
+  HTMLAnchorElement,
+  Omit<RouterLinkProps, 'to'> & { href: RouterLinkProps['to'] }
+>((props, ref) => {
+  const { href, ...other } = props;
+  // Map href (MUI) -> to (react-router)
+  return <RouterLink data-testid="custom-link" ref={ref} to={href} {...other} />;
+});
+
 const theme = createTheme({
     cssVariables: true,
     palette: {
@@ -16,6 +31,18 @@ const theme = createTheme({
             main: red.A400,
         },
     },
+    components: {
+    MuiLink: {
+      defaultProps: {
+        component: LinkBehavior,
+      } as LinkProps,
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        LinkComponent: LinkBehavior,
+      },
+    },
+  },
 });
 
 export default theme;

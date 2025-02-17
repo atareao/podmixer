7cdbfa26901f1fc6cf22fc175a4c31589d3fc002
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-02-17 21:27:37 +0100

    static

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/logo.svg',
+          dest: 'images'
+        }
+      ]
+    })
+  ],
 })
